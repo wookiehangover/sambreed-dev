58cd4848a3d012972f384a0922862942f746e66d
Sam Breed <samuel.breed@gmail.com>
2023-07-17 22:57:08 -0700

    update astro

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,16 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
+export async function get(context) {
+	const posts = await getCollection('blog');
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./writing/**/*.{md,mdx}'),
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/writing/${post.slug}`,
+		})),
 	});
+}
