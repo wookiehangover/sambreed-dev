36d7acef91a1d8ea3fed150ca062243e77e7d7e1
Sam Breed <samuel.breed@gmail.com>
2023-10-31 05:05:37 +0000

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,19 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { getCollection } from 'astro:content';
 
-export async function get(context) {
+export async function GET(context) {
+	const posts = await getCollection('writing')
+		.sort((a, b) => new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf())
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: await pagesGlobToRssItems(
-			import.meta.glob('./writing/**/*.{md,mdx}'),
-		)
+		items: posts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/writing/${post.slug}`
+		}))
 	});
 }
