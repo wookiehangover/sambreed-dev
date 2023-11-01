2a87fe721b353675d1cf85fa21515b6de900a39d
Sam Breed <samuel.breed@gmail.com>
2023-10-31 19:59:12 -0700

    full fat rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,33 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { getCollection } from 'astro:content';
+import { unified } from 'unified'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+	const posts = (await getCollection('writing'))
+		.sort((a, b) => new Date(b.data.pubDate).valueOf() - new Date(a.data.pubDate).valueOf())
+
+	const parser = await unified()
+		.use(remarkParse)
+		.use(remarkRehype)
+		.use(rehypeSanitize)
+		.use(rehypeStringify)
+
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site!,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			pubDate: new Date(post.data.pubDate),
+			description: post.data.description,
+			link: `/writing/${post.slug}`,
+			content: parser.processSync(post.body).toString()
+		}))
+	});
+}
