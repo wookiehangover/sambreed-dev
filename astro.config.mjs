2a58918946b16c400fd8fc5e2caaf02dd2fe6753
Sam Breed <samuel.breed@gmail.com>
2023-11-20 23:46:27 -0800

    add code highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkToc from 'remark-toc'
+import { visit } from 'unist-util-visit';
 
 // https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+
+function remarkCodeWrapperPlugin() {
+  return function transformer(tree) {
+    visit(tree, 'code', (node) => {
+      const rawContent = node.value;
+      const lang = node.lang ?? 'javascript';
+      node.type = 'html';
+      node.value = `<code-block lang="${lang}"><pre><code>${rawContent}</code></pre></code-block>`;
+    });
+    return tree;
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +26,7 @@ export default defineConfig({
   output: "server",
   adapter: cloudflare(),
   markdown: {
-    remarkPlugins: [remarkToc]
+    remarkPlugins: [remarkToc, remarkCodeWrapperPlugin]
   },
   vite: {
     ssr: {
