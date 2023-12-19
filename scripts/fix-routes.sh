15fc1006083557b459d7c95c381b24b3aadd2691
Sam Breed <samuel.breed@gmail.com>
2023-12-18 22:29:35 -0800

    fix routes

--- scripts/fix-routes.sh
+++ scripts/fix-routes.sh
@@ -6,9 +6,9 @@ FILE_NAME="dist/_routes.json"
 # Backup the original file before making changes
 cp "$FILE_NAME" "${FILE_NAME}.bak"
 
-jq '(.exclude | map(select(test("^/img/.*$|^/languages/.*$") | not))) as $newExcludes | .exclude = $newExcludes' "$FILE_NAME" > "$FILE_NAME.tmp" && mv "$FILE_NAME.tmp" "$FILE_NAME"
+jq '(.exclude | map(select(test("^/img/.*$|^/languages/.*$|^/_astro/.*$") | not))) as $newExcludes | .exclude = $newExcludes' "$FILE_NAME" > "$FILE_NAME.tmp" && mv "$FILE_NAME.tmp" "$FILE_NAME"
 
-jq '(.exclude += ["/img/*", "/languages/*"])' "$FILE_NAME" > "$FILE_NAME.tmp" && mv "$FILE_NAME.tmp" "$FILE_NAME"
+jq '(.exclude += ["/img/*", "/languages/*", "/_astro/*"])' "$FILE_NAME" > "$FILE_NAME.tmp" && mv "$FILE_NAME.tmp" "$FILE_NAME"
   
 # If there is no newline at the end of the file, add one
 tail -c1 "$FILE_NAME" | read -r _ || echo '' >> "$FILE_NAME"
