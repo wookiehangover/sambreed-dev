f6e08b6b262a4d942676577d9feef7352bae6376
Sam Breed <samuel.breed@gmail.com>
2024-08-03 20:37:57 -0700

    lint and format with biome

--- scripts/extract-links.mjs
+++ scripts/extract-links.mjs
@@ -1,39 +1,37 @@
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import rehypeStringify from 'rehype-stringify'
-import { visit } from 'unist-util-visit';
-import fs from 'fs/promises';
-import path from 'path';
-import { globby } from 'globby'
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import { visit } from "unist-util-visit";
+import fs from "fs:promises";
+import { globby } from "globby";
 
-const all_links = {}
+const all_links = {};
 
 const processMarkdownFile = async (filePath) => {
   try {
-    const fileContents = await fs.readFile(filePath, 'utf-8');
-    const links = []
-    const slug = filePath.replace('./src/content/writing/', '').replace('.mdx', '').replace('.md', '')
+    const fileContents = await fs.readFile(filePath, "utf-8");
+    const links = [];
+    const slug = filePath.replace("./src/content/writing/", "").replace(".mdx", "").replace(".md", "");
     const parser = unified()
       .use(remarkParse)
       .use(remarkRehype)
-      .use(() => tree => {
-        visit(tree, 'element', node => {
-          if (node.tagName === 'a') {
+      .use(() => (tree) => {
+        visit(tree, "element", (node) => {
+          if (node.tagName === "a") {
             const url = node.properties.href;
             const title = node.children[0].value;
-            if (url.startsWith('http')) {
+            if (url.startsWith("http")) {
               links.push({ title, url });
             }
           }
         });
       })
-      .use(rehypeStringify)
+      .use(rehypeStringify);
 
-      parser.processSync(fileContents)
+    parser.processSync(fileContents);
 
-      all_links[slug] = links;
-
+    all_links[slug] = links;
   } catch (error) {
     console.error(`Error processing file ${filePath}: ${error}`);
   }
@@ -41,9 +39,8 @@ const processMarkdownFile = async (filePath) => {
 
 const processDirectory = async (directory) => {
   try {
-    const files = await globby(directory)
+    const files = await globby(directory);
     for (const file of files) {
-      
       await processMarkdownFile(file);
     }
   } catch (error) {
@@ -51,6 +48,10 @@ const processDirectory = async (directory) => {
   }
 };
 
-await processDirectory('./src/content/writing/202*/**/*link*.md*');
-await fs.writeFile('./src/data/links.json', JSON.stringify(all_links), 'utf-8')
-console.log(`Extracted ${Object.values(all_links).reduce((acc, links) => acc + links.length, 0)} links from ${Object.keys(all_links).length} files`)
+await processDirectory("./src/content/writing/202*/**/*link*.md*");
+await fs.writeFile("./src/data/links.json", JSON.stringify(all_links), "utf-8");
+console.log(
+  `Extracted ${Object.values(all_links).reduce((acc, links) => acc + links.length, 0)} links from ${
+    Object.keys(all_links).length
+  } files`
+);
--- src/pages/api/random-link.ts
+++ src/pages/api/random-link.ts
@@ -1,30 +1,30 @@
 import type { APIRoute } from "astro";
 import links from "../../data/links.json";
 
-export const GET: APIRoute = async function ({ request }) {
-  
-  const url = new URL(request.url);
-  const query = url.searchParams.get("q") as string;
+export const GET: APIRoute = async ({ request }) => {
+	const url = new URL(request.url);
+	const query = url.searchParams.get("q") as string;
 
-  let allLinks = Object.values(links).flat();
+	let allLinks = Object.values(links).flat();
 
-  if (query) {
-    allLinks = allLinks.filter((link) =>
-      link.title.toLowerCase().includes(query.toLowerCase()) ||
-      link.url.toLowerCase().includes(query.toLowerCase()),
-    );
-  }
+	if (query) {
+		allLinks = allLinks.filter(
+			(link) =>
+				link.title.toLowerCase().includes(query.toLowerCase()) ||
+				link.url.toLowerCase().includes(query.toLowerCase()),
+		);
+	}
 
-  const randomLink = allLinks[Math.floor(Math.random() * allLinks.length)];
+	const randomLink = allLinks[Math.floor(Math.random() * allLinks.length)];
 
-  const destination = randomLink.url;
+	const destination = randomLink.url;
 
-  // redirect
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: destination,
-      ReferrerPolicy: "no-referrer",
-    },
-  });
-}
+	// redirect
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: destination,
+			ReferrerPolicy: "no-referrer",
+		},
+	});
+};
